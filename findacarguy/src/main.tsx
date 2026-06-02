ee53581c065ed78d92ae3e2fdb8f157b8de9b700
gibbssonofgobbs <charlesbgibbs@gmail.com>
2026-06-02 13:47:21 -0400

    Update main.tsx

--- findacarguy/src/main.tsx
+++ findacarguy/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import FindACarGuy from './FindACarGuy'
+import NotFound from './NotFound'
+
+function App() {
+  const path = window.location.pathname
+  if (path === '/' || path === '/index.html') return <FindACarGuy />
+  return <NotFound />
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <FindACarGuy />
+    <App />
   </React.StrictMode>
 )
